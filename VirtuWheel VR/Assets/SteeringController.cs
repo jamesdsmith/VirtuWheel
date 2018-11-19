41ad3745c2fefcc05dc0bde967d92d664a677dd8
James Smith <codefiend@gmail.com>
2018-11-19 15:25:53 -0800

    Adding code to link up to Thrustmaster steering wheel

---
+++ VirtuWheel VR/Assets/SteeringController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteeringController : MonoBehaviour
+{
+    public GameObject wheelObject;
+    private ThrustmasterWheel wheel;
+    private float lastRotation = 0;
+
+    void Start()
+    {
+        if (wheelObject != null)
+        {
+            wheel = wheelObject.GetComponent<ThrustmasterWheel>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wheel != null)
+        {
+            float rot = Input.GetAxis("Wheel Angle") * 450;
+            wheel.wheel.transform.RotateAround(wheel.wheel.transform.position,
+                wheel.wheel.transform.up,
+                rot - lastRotation);
+            lastRotation = rot;
+        }
+    }
+}
---
+++ VirtuWheel VR/Assets/ThrustmasterWheel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrustmasterWheel : MonoBehaviour
+{
+    public GameObject body;
+    public GameObject wheel;
+}
